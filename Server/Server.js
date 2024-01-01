698c36a4a4da5a98a793317f03452bd9794a5a1c
JaredLove <lovejared91@Yahoo.com>
2024-01-01 12:58:20 -0600

    added logic for signup and logging in

--- Server/Server.js
+++ Server/Server.js
@@ -3,7 +3,8 @@ const express = require('express');
 const session = require('express-session');
 
 
-
+// instantiate the server
+// We assign the express() function to the app variable so that we can later chain on methods to the Express.js server. This essentially saves us from writing the same code over and over again.
 const app = express();
 const PORT = 3001;
 
@@ -21,11 +22,16 @@ const sess = {
     })
   };
 
+// turn on sessions
 app.use(session(sess));
+// parse incoming JSON data
 app.use(express.json());
+// parse incoming string or array data
 app.use(express.urlencoded({ extended: false }));
+// serve up static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// turn on routes
 app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
